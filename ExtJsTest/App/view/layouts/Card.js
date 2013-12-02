103a7a97bb5e047ec9ac77d8a9d68e6fa8d7d2ae
AlvaroRomero <alvaro@dvloop.com>
2013-12-02 17:43:12 -0300

    cambia Master

--- ExtJsTest/App/view/layouts/Card.js
+++ ExtJsTest/App/view/layouts/Card.js
@@ -71,7 +71,7 @@
 		},
 		{
 			id: 'card-2',
-			html: '<h1>Felicitaciones!</h1><p>Paso 3 of 3 - Complete</p>'
+			html: '<h1>Felicitaciones!</h1><p>Paso 3 of 3 - ahora si cambia el master</p>'
 		}
 	],
 
